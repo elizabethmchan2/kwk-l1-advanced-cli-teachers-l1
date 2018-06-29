0ea86f10d57734792e0cd82be6ff4ca7e487b132
Elizabeth Chan <lmchan@brynmawr.edu>
2018-06-29 18:12:35 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,4 +1,10 @@
 mv delivery/apples.txt fruit
 mv delivery/bison_burgers.txt meat
 mv delivery/brie.txt dairy
-mv
+mv delivery/broccoli.txt veggies
+mv delivery/carrot_cake.txt dessert
+mv delivery/eggo_waffles.txt freezer
+mv delivery/fage_yogurt.txt dairy
+mv delivery/fig_bars.txt snax
+mv delivery/flourless_cake dessert
+mv garlic.txt 
